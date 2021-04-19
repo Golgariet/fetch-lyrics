ae5f82618abefaa5e17130d322de7a79035a7298
Anton Bibergal <nathgilt@gmail.com>
2021-04-19 20:58:00 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -3,24 +3,85 @@ const search = document.getElementById('search');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
 
-const URL = 'https://api.lyrics.ovh';
+const apiURL = 'https://api.lyrics.ovh';
 
 async function searchSongs(term) {
-  const res = await fetch(`${URL}/suggest/${term}`);
+  const res = await fetch(`${apiURL}/suggest/${term}`);
   const data = await res.json();
 
   showData(data);
 }
 
-function showData(data) {}
+function showData(data) {
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+        .map(
+          (song) => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`
+        )
+        .join('')}
+    </ul>
+  `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMore('${data.prev}')">Prev</button>`
+          : ''
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMore('${data.next}')">Next</button>`
+          : ''
+      }
+    `;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+async function getMore(url) {
+  const res = await fetch(`https://cors.bridged.cc/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+  more.innerHTML = '';
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const searchTerm = search.value.trim();
+
   if (!searchTerm) {
-    alert('Please type in a search term.');
+    alert('Please type in a search term');
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+result.addEventListener('click', (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
   }
 });
